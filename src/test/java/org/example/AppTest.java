4eaad7c73df0bcea20f0fabfb3a2a4cd11c1e754
sanu877 <sanusharma8774@gmail.com>
2025-06-25 22:31:35 +0530

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     *
+     *
+     *
+     */
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+    }
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
