ccef54cf8326e9d8b14b0832a3c331e763864133
sanu877 <sanusharma8774@gmail.com>
2025-06-25 22:45:30 +0530

    this is from tofiue

--- src/test/java/org/example/SecondTest.java
+++ src/test/java/org/example/SecondTest.java
@@ -4,5 +4,8 @@ public class SecondTest {
 
     public static void main(String[] args) {
         System.out.println("this is testing");
+
+        System.out.println("this is my second");
+        System.out.println("ki korchis");
     }
 }
