66e7a6e5bae329d4047f3f6fd6734625d9557699
sanu877 <sanusharma8774@gmail.com>
2025-06-29 23:25:12 +0530

    my changes

---
+++ src/test/java/org/example/FifthTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FifthTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is small change");
+    }
+}
---
+++ src/test/java/org/example/FourthTST.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FourthTST {
+
+    public static void main(String[] args) {
+        System.out.println("this is architecture change");
+
+    }
+}
